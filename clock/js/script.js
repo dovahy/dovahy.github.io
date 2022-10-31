bd953981522c8801aa2ecd6d09be399271fea82e
Florian Swinareck <floriancartman@hotmail.de>
2022-10-31 18:15:05 +0100

    Update other project pages

--- clock/js/script.js
+++ clock/js/script.js
@@ -1,4 +1,5 @@
 setInterval(setClock, 1000)
+setInterval(setDigClock, 10)
 
 const hourHand = document.querySelector('[data-hour-hand')
 const minuteHand = document.querySelector('[data-minute-hand')
@@ -19,4 +20,27 @@ function setRotation(element, rotationRatio)
     element.style.setProperty('--rotation', rotationRatio * 360)
 }
 
+function setDigClock() {
+    var currentDate = new Date();
+    var hrs = currentDate.getHours();
+    var min = currentDate.getMinutes();
+    var sec = currentDate.getSeconds();
+    //var session = document.getElementById('session');
+
+    //if(hrs >= 12) {
+    //    session.innerHTML = 'PM';
+    //} else {
+    //session.innerHTML = 'AM';
+    //}
+
+    hrs = (hrs < 10) ? "0" + hrs : hrs;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    document.getElementById('dighours').innerHTML = hrs;
+    document.getElementById('digminutes').innerHTML = min;
+    document.getElementById('digseconds').innerHTML = sec;
+}
+
 setClock()
+setDigClock()

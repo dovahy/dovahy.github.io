2e7f016f66fae44f830bf70602537e1953b5fffb
Florian Swinareck <floriancartman@hotmail.de>
2022-12-01 19:34:53 +0100

    update fix
    z index is weird

--- new/js/script.js
+++ new/js/script.js
@@ -1,80 +1,31 @@
-// Carousel code
+var icon = document.getElementById('mode-icon');
 
-const carousel = document.querySelector(".image-carousel"),
-firstImg = carousel.querySelectorAll("img")[0],
-arrowIcons = document.querySelectorAll(".section-project-images i");
-
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
-
-const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
-    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+icon.onclick = function(){
+    document.body.classList.toggle('dark-theme')
 }
 
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-        carousel.scrollLeft += icon.id == "carousel-left-arrow" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-    });
+function changeColor() {
+    var scrollValue = window.scrollY;
+    var header = document.getElementById('topnav-right');
+    console.log(scrollValue)
+    if (scrollValue < 790)
+    {
+        header.classList.remove('changedColor');
+    } else
+    {
+        header.classList.add('changedColor');
+    }
+}
+
+window.addEventListener('scroll', changeColor);
+
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
 });
 
-const autoSlide = () => {
-    // if there is no image left to scroll then return from here
-    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
-    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
-    let firstImgWidth = firstImg.clientWidth + 14;
-    // getting difference value that needs to add or reduce from carousel left to take middle img center
-    let valDifference = firstImgWidth - positionDiff;
-    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
-        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-    }
-    // if user is scrolling to the left
-    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-}
-
-const dragStart = (e) => {
-    // updatating global variables value on mouse down event
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
-}
-
-const dragging = (e) => {
-    // scrolling images/carousel to left according to mouse pointer
-    if(!isDragStart) return;
-    e.preventDefault();
-    isDragging = true;
-    carousel.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-    showHideIcons();
-}
-
-const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
-    if(!isDragging) return;
-    isDragging = false;
-    autoSlide();
-}
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
-document.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);
-
-//const fixedHeader = document.querySelector('.header-wrap');
-//
-//const stickyHeader = window.addEventListener('scroll', () => {
-//    if (window.scrollY < 300) {
-//        fixedHeader.classList.add('sticky');
-//    } else {
-//        fixedHeader.classList.remove('sticky');
-//    }
-//});
+ScrollReveal().reveal('.about-main-title', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.content-image', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.content-text', { delay: 700, origin: 'right' });
